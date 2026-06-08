6a45910b23e627d9d001b0ea9cf97797eae521bd
Jorge Ricardo SILVA ZUÑIGA <jsilvazuniga@gmail.com>
2026-06-08 20:59:01 +0200

    create logger

---
+++ Python/src/logger/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+# configure logging 
+logging.basicConfig(
+    filename='app.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
